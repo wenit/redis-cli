6c0e6bf841ac6dfbc29fb6b0df4ef3e8ec68cf24
章文兵 <zwb@yitong.com.cn>
2020-05-13 21:46:04 +0800

    优化章节目录结构

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# redis-cli
-手把手教你写redis客户端
+# 手把手教你写redis客户端
+
+## 目录
+- [x] [01-命令行输入](chapter01/README.md)
+- [ ] [02-建立网络连接](chapter02/README.md)
+- [ ] [03-请求报文组装](chapter03/README.md)
+- [ ] [04-响应报文解析](chapter04/README.md)
+- [ ] [05-命令行自动完成](chapter05/README.md)
+- [ ] [06-连接池实现](chapter06/README.md)
--- redis-cli.go
+++ redis-cli.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net"
 	"os"
 )
 
@@ -17,38 +16,24 @@ func init() {
 	flag.IntVar(&port, "p", 6379, "port")
 }
 
+// 程序入口
 func main() {
 	// 解析命令行参数
 	flag.Parse()
 
-	conn := NewConn()
-
-	loop(conn)
-
-	defer conn.Close()
+	loop()
 }
 
-func loop(conn net.Conn) {
+// 命令行
+func loop() {
 	for {
 		fmt.Printf("%s:%d>", host, port)
 		bio := bufio.NewReader(os.Stdin)
+		// 获取命令行输入
 		input, _, err := bio.ReadLine()
 		if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Printf("%s\n", input)
 	}
-}
-
-// 创建链接
-func NewConn() net.Conn {
-
-	addr := &net.TCPAddr{IP: net.ParseIP(host), Port: port}
-	conn, err := net.DialTCP("tcp", nil, addr)
-
-	if err != nil {
-		log.Fatal("初始化链接失败：", err)
-	}
-
-	return conn
 }
